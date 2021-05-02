be340f6373a97f03d12e39f2328616cfe7a37721
kurlitahymro <kurlitahymro@163.com>
2021-05-03 04:36:13 +0800

    Component

---
+++ Source/BattleField/Private/Character/BattlefieldCharacterBase.cpp
@@ -0,0 +1,279 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/BattlefieldCharacterBase.h"
+#include "Common/CommonInterface.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+ABattlefieldCharacterBase::ABattlefieldCharacterBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	bUseControllerRotationYaw = false;
+	bIsValid = false;
+
+	// Set size for collision capsule
+	GetCapsuleComponent()->InitCapsuleSize(50.f, 90.f);
+
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 400.0f, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->JumpZVelocity = 400.f;
+	GetCharacterMovement()->AirControl = 0.2f;
+	GetCharacterMovement()->bUseControllerDesiredRotation = true;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	ComponentInit();
+	bIsValid = true;
+}
+
+// Called when the game starts or when spawned
+void ABattlefieldCharacterBase::BeginPlay()
+{
+	BeginPlayLoad();
+
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABattlefieldCharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ABattlefieldCharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+UActorState* ABattlefieldCharacterBase::GetState()
+{
+	if (State) {
+		return State;
+	} else {
+		UE_LOG(LoadLog, Error, TEXT("CharacterBase No State"));
+		return nullptr;
+	}
+}
+
+void ABattlefieldCharacterBase::SetCharacterSkeletalMesh(USkeletalMesh* sk, FTransform transform)
+{
+	if (sk) {
+		GetMesh()->SetSkeletalMesh(sk);
+		GetMesh()->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		GetMesh()->SetRelativeTransform(transform);
+	} else {
+		UE_LOG(LoadLog, Error, TEXT("Load SkeletalMesh Error"));
+	}
+}
+
+void ABattlefieldCharacterBase::SetCharacterWeaponMesh(USkeletalMesh* weapon, FTransform transform)
+{
+	if (weapon && MainWeapon) {
+		MainWeapon->SetSkeletalMesh(weapon);
+		MainWeapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+		MainWeapon->SetRelativeTransform(transform);
+
+		WeaponMesh = MainWeapon;
+	} else {
+		UE_LOG(LoadLog, Error, TEXT("Load Weapon Error"));
+	}
+}
+
+void ABattlefieldCharacterBase::SetCharacterAnimBlueprint(const UAnimBlueprint* abp)
+{
+	UClass* abpClass = abp->GetAnimBlueprintGeneratedClass();
+	GetMesh()->SetAnimationMode(EAnimationMode::AnimationBlueprint);
+	GetMesh()->SetAnimInstanceClass(abpClass);
+}
+
+void ABattlefieldCharacterBase::HitActor(AActor* targetActor)
+{
+	UGameplayStatics::ApplyDamage(targetActor,
+		CalDamage(),
+		this->GetController(),
+		this,
+		DamageTypeClass);
+	return;
+}
+
+void ABattlefieldCharacterBase::ComponentInit()
+{
+	/* step1 : 创建组件 */
+	/* 1.1 状态管理组件 */
+	State = CreateDefaultSubobject<UActorState>(TEXT("State"));
+	if (State) {
+		RunSpeedChangeValue = 300;
+	} else {
+		bIsValid = false; // 组件内存申请失败时角色不可用。
+		return;
+	}
+
+	// TODO: 做一个装备的表，先读取衣服，生成新的SkMesh；再读武器，绑到对应插槽上或隐藏。
+	/* 1.2 Mesh组件 */
+	if (GetMesh()) {
+	} else {
+		bIsValid = false; // 组件内存申请失败时角色不可用。
+		return;
+	}
+	USkeletalMesh* skMesh = LoadObject<USkeletalMesh>(NULL,
+		TEXT("SkeletalMesh'/Game/Animation/SkeletalMesh/SK_Default.SK_Default'"));
+	FTransform tBody = UKismetMathLibrary::MakeTransform(
+		FVector(0.f, 0.f, -90.f),
+		FRotator(0.f, -90.f, 0.f),
+		FVector(1.f, 1.f, 1.f));
+	SetCharacterSkeletalMesh(skMesh, tBody);
+
+	/* 1.3 武器管理组件 */
+	MainWeapon = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("MainWeapon"));
+	if (MainWeapon) {
+		MainWeapon->SetupAttachment(GetMesh(), TEXT("main_hand"));
+	} else {
+		bIsValid = false; // 组件内存申请失败时角色不可用。
+		return;
+	}
+	USkeletalMesh* axc = LoadObject<USkeletalMesh>(NULL,
+		TEXT("SkeletalMesh'/Game/Character/Mesh/Animation/heavy_weapons/SK_Axc.SK_Axc'"));
+	FTransform tWeapon = UKismetMathLibrary::MakeTransform(
+		FVector(0.f, 0.f, -20.f),
+		FRotator(0.f, 0.f, 0.f),
+		FVector(1.f, 1.f, 0.8f));
+	SetCharacterWeaponMesh(axc, tWeapon);
+}
+
+void ABattlefieldCharacterBase::BeginPlayLoad()
+{
+	/* step2 : 加载 */
+	/* 加载动画蓝图 */
+	UAnimBlueprint* abp = LoadObject<UAnimBlueprint>(NULL,
+		TEXT("AnimBlueprint'/Game/Animation/AnimBlueprint/ABP_Default.ABP_Default'"));
+	SetCharacterAnimBlueprint(abp);
+
+	/* 加载控件蓝图（蓝图层） */
+	CreateStateWidget();
+}
+
+float ABattlefieldCharacterBase::CalDamage()
+{
+	int damage = State->GetStateItem(EnumActorStateItem::EN_ATK);
+	int reduce = State->GetStateItem(EnumActorStateItem::EN_DEF);
+	return static_cast<float>(damage - reduce);
+}
+
+void ABattlefieldCharacterBase::CharacterStateUpdate_Implementation(EnumActorStateItem state)
+{
+	// 角色状态变更时，通知控件组件更新。
+}
+
+void ABattlefieldCharacterBase::CreateStateWidget_Implementation()
+{
+	// 玩家角色添加视口UI。
+	// AI角色添加头顶UI。
+}
+
+void ABattlefieldCharacterBase::AxisInput(EnumCharacterAxisAction axis, float value)
+{
+	switch (axis)
+	{
+	case EnumCharacterAxisAction::EN_MOVE_Y:
+		MoveForward(value);
+		break;
+	case EnumCharacterAxisAction::EN_MOVE_X:
+		MoveRight(value);
+		break;
+	case EnumCharacterAxisAction::EN_LOOK_Y:
+		LookUpAtRate(value);
+		break;
+	case EnumCharacterAxisAction::EN_LOOK_X:
+		TurnAtRate(value);
+		break;
+	}
+}
+
+void ABattlefieldCharacterBase::OnceInput(EnumCharacterOnceAction action, float value)
+{
+	switch (action)
+	{
+	case EnumCharacterOnceAction::EN_ATTACK:
+		break;
+	case EnumCharacterOnceAction::EN_JUMP:
+		JumpStarted();
+		break;
+	case EnumCharacterOnceAction::EN_JUMP_STOP:
+		JumpStopped();
+		break;
+	case EnumCharacterOnceAction::EN_SPEED_PLUS:
+		ChangeSpeed(RunSpeedChangeValue);
+		break;
+	case EnumCharacterOnceAction::EN_SPEED_SUB:
+		ChangeSpeed(-RunSpeedChangeValue);
+		break;
+	}
+}
+
+void ABattlefieldCharacterBase::MoveForward(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void ABattlefieldCharacterBase::MoveRight(float Value)
+{
+	if ((Controller != nullptr) && (Value != 0.0f))
+	{
+		// find out which way is right
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get right vector 
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		// add movement in that direction
+		AddMovementInput(Direction, Value);
+	}
+}
+
+void ABattlefieldCharacterBase::TurnAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerYawInput(Rate * BaseTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ABattlefieldCharacterBase::LookUpAtRate(float Rate)
+{
+	// calculate delta for this frame from the rate information
+	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
+}
+
+void ABattlefieldCharacterBase::JumpStarted()
+{
+	Jump();
+}
+
+void ABattlefieldCharacterBase::JumpStopped()
+{
+	StopJumping();
+}
+
+void ABattlefieldCharacterBase::ChangeSpeed(int spd)
+{
+	GetState()->SpeedChange(spd);
+}
+
+void ABattlefieldCharacterBase::ResetSpeed()
+{
+	GetState()->ResetSpeed();
+}
---
+++ Source/BattleField/Private/Component/ActorSpawner.cpp
@@ -0,0 +1,61 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Component/ActorSpawner.h"
+#include "NavigationSystem.h"
+
+// Sets default values for this component's properties
+UActorSpawner::UActorSpawner()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UActorSpawner::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UActorSpawner::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
+AActor* UActorSpawner::SpawnByInfo(UClass* actorClass, float r)
+{
+	UWorld* const world = GetWorld();
+	if (world) {
+		/* 获取导航 */
+		if (!navSys) {
+			navSys = UNavigationSystemV1::GetCurrent(world);
+		}
+		/* 计算位置 */
+		FNavLocation targetLocation;
+		bool readyToSpawn =
+			navSys->GetRandomReachablePointInRadius(GetOwner()->GetActorLocation(), r, targetLocation);
+		if (!readyToSpawn) {
+			return nullptr;
+		}
+		FVector toSpawnLocation = targetLocation.Location;
+		FRotator toSpawnRotation = GetOwner()->GetActorRotation();
+		/* 生成 */
+		AActor* spawnCharacter =
+			GetWorld()->SpawnActor(actorClass, &toSpawnLocation, &toSpawnRotation);
+		if (!spawnCharacter) {
+			return nullptr;
+		}
+	}
+	return nullptr;
+}
