04db95cf962d0aedece8df3c2615a37c8bd700a0
kurlitahymro <kurlitahymro@163.com>
2021-06-05 20:04:09 +0800

    第三人称控制组件

---
+++ Plugins/GameFeatures/ThirdPersonControls/Source/ThirdPersonControls/Public/ThirdPersonControlsComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Input/Components/PawnControlsComponent.h"
+#include "ThirdPersonControlsComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THIRDPERSONCONTROLS_API UThirdPersonControlsComponent : public UPawnControlsComponent
+{
+	GENERATED_BODY()
+	
+};
