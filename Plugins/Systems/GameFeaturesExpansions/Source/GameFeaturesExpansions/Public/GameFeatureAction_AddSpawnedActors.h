f883e7431246c03d96565a047b9c793421b323c4
kurlitahymro <kurlitahymro@163.com>
2021-06-12 01:56:13 +0800

    AddSpawnedActors

--- Plugins/Systems/GameFeaturesExpansions/Source/GameFeaturesExpansions/Public/GameFeatureAction_AddSpawnedActors.h
+++ Plugins/Systems/GameFeaturesExpansions/Source/GameFeaturesExpansions/Public/GameFeatureAction_AddSpawnedActors.h
@@ -6,6 +6,37 @@
 #include "GameFeatureAction.h"
 #include "GameFeatureAction_AddSpawnedActors.generated.h"
 
+/** Record for the an actor to spawn along with a game feature data. */
+USTRUCT()
+struct FSpawningActorEntry
+{
+	GENERATED_BODY()
+
+	// What kind of actor to spawn
+	UPROPERTY(EditAnywhere, Category = "Actor")
+	TSubclassOf<AActor> ActorType;
+
+	// Where to spawn the actor
+	UPROPERTY(EditAnywhere, Category = "Actor|Transform")
+	FTransform SpawnTransform;
+};
+
+/** Record for the game feature data. Specifies which actors to spawn for target worlds. */
+USTRUCT()
+struct FSpawningWorldActorsEntry
+{
+	GENERATED_BODY()
+
+	// The world to spawn the actors for (can be left blank, in which case we'll spawn them for all worlds)
+	UPROPERTY(EditAnywhere, Category = "Feature Data")
+	TSoftObjectPtr<UWorld> TargetWorld;
+
+	// The actors to spawn
+	UPROPERTY(EditAnywhere, Category = "Feature Data")
+	TArray<FSpawningActorEntry> Actors;
+
+};
+
 /**
  * 
  */
@@ -18,12 +49,30 @@ public:
 	//~ Begin UGameFeatureAction interface
 	virtual void OnGameFeatureActivating() override;
 	virtual void OnGameFeatureDeactivating(FGameFeatureDeactivatingContext& Context) override;
+
+#if WITH_EDITORONLY_DATA
+	virtual void AddAdditionalAssetBundleData(FAssetBundleData& AssetBundleData) override;
+#endif
 	//~ End UGameFeatureAction interface
+
+	//~ Begin UObject interface
+#if WITH_EDITOR
+	virtual EDataValidationResult IsDataValid(TArray<FText>& ValidationErrors) override;
+#endif
+	//~ End UObject interface
+
+	/** List of actors */
+	UPROPERTY(EditAnywhere, Category = "Actor")
+	TArray<FSpawningWorldActorsEntry> ActorsList;
 
 private:
 	void HandleGameInstanceStart(UGameInstance* GameInstance);
-	virtual void AddToWorld(const FWorldContext& WorldContext) PURE_VIRTUAL(UGameFeatureAction_WorldActionBase::AddToWorld,);
 
+	void AddToWorld(const FWorldContext& WorldContext);
+
+	void Reset();
 private:
 	FDelegateHandle GameInstanceStartHandle;
+
+	TArray<TWeakObjectPtr<AActor>> SpawnedActors;
 };
