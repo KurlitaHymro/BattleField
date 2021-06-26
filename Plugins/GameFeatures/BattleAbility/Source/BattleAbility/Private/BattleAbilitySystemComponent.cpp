07053da7cbe87c00b94027b2da544f7f227f66c6
kurlitahymro <kurlitahymro@163.com>
2021-06-27 01:43:44 +0800

    能力/属性的生成/清除

--- Plugins/GameFeatures/BattleAbility/Source/BattleAbility/Private/BattleAbilitySystemComponent.cpp
+++ Plugins/GameFeatures/BattleAbility/Source/BattleAbility/Private/BattleAbilitySystemComponent.cpp
@@ -3,3 +3,57 @@
 
 #include "BattleAbilitySystemComponent.h"
 
+void UBattleAbilitySystemComponent::ClearAddedAbilities()
+{
+	for (const auto Ability : AddedAbilityHandle)
+	{
+		SetRemoveAbilityOnEnd(Ability);
+	}
+	AddedAbilityHandle.Empty(DefaultAbilities.Num());
+}
+
+void UBattleAbilitySystemComponent::ClearAddedAttributes()
+{
+	for (const auto Attribute : AddedAttributeSet)
+	{
+		GetSpawnedAttributes_Mutable().Remove(Attribute);
+	}
+	AddedAttributeSet.Empty(DefaultAttributes.Num());
+}
+
+void UBattleAbilitySystemComponent::GrantDefaultAbilities()
+{
+	AddedAbilityHandle.Reserve(DefaultAbilities.Num());
+	for (const auto &Ability : DefaultAbilities)
+	{
+		if (Ability.AbilityType)
+		{
+			AddedAbilityHandle.Add(GiveAbility(FGameplayAbilitySpec(Ability.AbilityType)));
+		}
+	}
+}
+
+void UBattleAbilitySystemComponent::GrantDefaultAttributes()
+{
+	AddedAttributeSet.Reserve(DefaultAttributes.Num());
+	for (const auto& Attribute : DefaultAttributes)
+	{
+		if (Attribute.AttributeSetType) {
+			UAttributeSet* AttributeSet = NewObject<UAttributeSet>(this, Attribute.AttributeSetType);
+			if (Attribute.InitializationData)
+			{
+				AttributeSet->InitFromMetaDataTable(Attribute.InitializationData);
+			}
+			AddedAttributeSet.Add(AttributeSet);
+			AddAttributeSetSubobject(AttributeSet);
+		}
+	}
+}
+
+void UBattleAbilitySystemComponent::GrantDefaultAbilitiesAndAttributes()
+{
+	ClearAddedAbilities();
+	ClearAddedAttributes();
+	GrantDefaultAbilities();
+	GrantDefaultAttributes();
+}
--- Plugins/GameFeatures/BattleAbility/Source/BattleAbility/Public/BattleAbilitySystemComponent.h
+++ Plugins/GameFeatures/BattleAbility/Source/BattleAbility/Public/BattleAbilitySystemComponent.h
@@ -5,6 +5,36 @@
 #include "CoreMinimal.h"
 #include "AbilitySystemComponent.h"
 #include "BattleAbilitySystemComponent.generated.h"
+ 
+/**
+ * ƒ‹¡¶
+ */
+USTRUCT()
+struct FBattleAbilityApplication
+{
+	GENERATED_BODY()
+
+	// Ability set to grant
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UGameplayAbility> AbilityType;
+};
+
+/**
+ *  Ù–‘
+ */
+USTRUCT()
+struct FBattleAttributeApplication
+{
+	GENERATED_BODY()
+
+	// Ability set to grant
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UAttributeSet> AttributeSetType;
+
+	// Data table reference to initialize the attributes with, if any (can be left unset)
+	UPROPERTY(EditAnywhere)
+	UDataTable* InitializationData = nullptr;
+};
 
 /**
  * 
@@ -14,4 +44,23 @@ class BATTLEABILITY_API UBattleAbilitySystemComponent : public UAbilitySystemCom
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditDefaultsOnly, Category = Ability)
+	TArray<FBattleAbilityApplication> DefaultAbilities;
+
+	UPROPERTY(EditDefaultsOnly, Category = Ability)
+	TArray<FBattleAttributeApplication> DefaultAttributes;
+
+protected:
+	UPROPERTY(transient)
+	TArray<struct FGameplayAbilitySpecHandle> AddedAbilityHandle;
+
+	UPROPERTY(transient)
+	TArray<class UAttributeSet*> AddedAttributeSet;
+
+	void ClearAddedAbilities();
+	void ClearAddedAttributes();
+	void GrantDefaultAbilities();
+	void GrantDefaultAttributes();
+	void GrantDefaultAbilitiesAndAttributes();
 };
