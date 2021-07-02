aa01f640eed00a6a428d7a5a2fcb6f0cf910a4b8
kurlitahymro <kurlitahymro@163.com>
2021-07-03 00:55:41 +0800

    能力系统组件

--- Plugins/AbilityAttributeSystem/BattleAbilityAttribute/Source/BattleAbilityAttribute/Public/BattleSystemComponent.h
+++ Plugins/AbilityAttributeSystem/BattleAbilityAttribute/Source/BattleAbilityAttribute/Public/BattleSystemComponent.h
@@ -7,11 +7,70 @@
 #include "BattleSystemComponent.generated.h"
 
 /**
+ * ƒ‹¡¶
+ */
+USTRUCT()
+struct FBattleAbilityApplication
+{
+	GENERATED_BODY()
+
+	// Ability set to grant
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UGameplayAbility> AbilityType;
+};
+
+/**
+ *  Ù–‘
+ */
+USTRUCT()
+struct FBattleAttributeApplication
+{
+	GENERATED_BODY()
+
+	// Ability set to grant
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<class UAttributeSet> AttributeSetType;
+
+	// Data table reference to initialize the attributes with, if any (can be left unset)
+	UPROPERTY(EditAnywhere)
+	UDataTable* InitializationData = nullptr;
+};
+
+/**
  * 
  */
-UCLASS()
+UCLASS(meta = (BlueprintSpawnableComponent))
 class BATTLEABILITYATTRIBUTE_API UBattleSystemComponent : public UAbilitySystemComponent
 {
 	GENERATED_BODY()
 	
+public:
+	UPROPERTY(EditDefaultsOnly, Category = Ability)
+	TArray<FBattleAbilityApplication> DefaultAbilities;
+
+	UPROPERTY(EditDefaultsOnly, Category = Ability)
+	TArray<FBattleAttributeApplication> DefaultAttributes;
+
+protected:
+	UPROPERTY(transient)
+	TArray<struct FGameplayAbilitySpecHandle> AddedAbilityHandle;
+
+	UPROPERTY(transient)
+	TArray<class UAttributeSet*> AddedAttributeSet;
+
+	void ClearAddedAbilities();
+
+	void ClearAddedAttributes();
+
+	void GrantDefaultAbilities();
+
+	void GrantDefaultAttributes();
+
+public:
+	virtual void InitAbilityActorInfo(AActor* InOwnerActor, AActor* InAvatarActor) override;
+
+	virtual void BeginDestroy() override;
+
+	UFUNCTION()
+	void OnPawnControllerChanged(APawn* Pawn, AController* NewController);
 };
