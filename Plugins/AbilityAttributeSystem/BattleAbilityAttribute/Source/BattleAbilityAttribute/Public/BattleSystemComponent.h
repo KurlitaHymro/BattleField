97eb71a8253744bf6f0f90cb77ef158fd5b8be80
kurlitahymro <kurlitahymro@163.com>
2021-07-03 00:31:22 +0800

    能力插件重构

---
+++ Plugins/AbilityAttributeSystem/BattleAbilityAttribute/Source/BattleAbilityAttribute/Public/BattleSystemComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "BattleSystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATTLEABILITYATTRIBUTE_API UBattleSystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
