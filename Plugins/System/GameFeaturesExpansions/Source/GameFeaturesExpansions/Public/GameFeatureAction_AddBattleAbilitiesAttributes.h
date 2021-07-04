25387f14d66d86c613d23277742d95f13b58d2cb
kurlitahymro <kurlitahymro@163.com>
2021-07-04 20:16:20 +0800

    rename

---
+++ Plugins/System/GameFeaturesExpansions/Source/GameFeaturesExpansions/Public/GameFeatureAction_AddBattleAbilitiesAttributes.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFeatureAction.h"
+#include "GameFeatureAction_AddBattleAbilitiesAttributes.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GAMEFEATURESEXPANSIONS_API UGameFeatureAction_AddBattleAbilitiesAttributes : public UGameFeatureAction
+{
+	GENERATED_BODY()
+	
+};
