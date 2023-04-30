d4c40eb8eb573142078afaa543bc581253eb2e2f
kurlitahymro <kurlitahymro@163.com>
2023-05-01 06:02:01 +0800

    动作调整

--- Plugins/GameFeatures/Battle/BattleFrame/Source/BattleFrameRuntime/Public/EquipmentSystem/MoveDamageWeapon.h
+++ Plugins/GameFeatures/Battle/BattleFrame/Source/BattleFrameRuntime/Public/EquipmentSystem/MoveDamageWeapon.h
@@ -14,7 +14,7 @@ class BATTLEFRAMERUNTIME_API AMoveDamageWeapon : public AEquipBase
 {
 	GENERATED_BODY()
 
-	friend class UAnimNotifyState_BattleAnimHit;
+	friend class UAnimNotifyState_HitPointTrace;
 
 	UPROPERTY(Category = "Weapon", EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = "true", DisplayName = "HitPoint Size"))
 	TMap<FName, FVector> HitPoints;
