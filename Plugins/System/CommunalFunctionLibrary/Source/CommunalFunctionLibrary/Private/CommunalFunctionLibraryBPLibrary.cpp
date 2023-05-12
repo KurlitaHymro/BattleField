72e4a3f794fe4cde0a492fbffdb60932c40bd10d
kurlitahymro <kurlitahymro@163.com>
2023-05-12 22:54:08 +0800

    人防

---
+++ Plugins/System/CommunalFunctionLibrary/Source/CommunalFunctionLibrary/Private/CommunalFunctionLibraryBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CommunalFunctionLibraryBPLibrary.h"
+#include "CommunalFunctionLibrary.h"
+
+UCommunalFunctionLibraryBPLibrary::UCommunalFunctionLibraryBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UCommunalFunctionLibraryBPLibrary::CommunalFunctionLibrarySampleFunction(float Param)
+{
+	return -1;
+}
+
---
+++ Plugins/System/CommunalFunctionLibrary/Source/CommunalFunctionLibrary/Public/NavigationSystemAdapter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "NavigationSystemAdapter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMMUNALFUNCTIONLIBRARY_API UNavigationSystemAdapter : public UObject
+{
+	GENERATED_BODY()
+	
+};
